df01c9fe4a7282ddd52211339b71174207bff50f
Carlos <dominguez5493.cd@gmail.com>
2025-03-10 13:08:39 -0500

    first commit

--- api/config/server.ts
+++ api/config/server.ts
@@ -1,6 +1,11 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 10000),
+  url: env('PUBLIC_URL', 'https://apisantastortas-production.up.railway.app'),
+  admin: {
+    url: env('PUBLIC_URL', 'https://apisantastortas-production.up.railway.app'),
+    serveAdminPanel: true,
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
