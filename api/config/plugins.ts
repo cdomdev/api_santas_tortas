7b44948ceedf3c40b1344983edd57fc1579a2a71
Carlos <dominguez5493.cd@gmail.com>
2025-03-16 10:47:16 -0500

    refactor: remove provider google

--- api/config/plugins.ts
+++ api/config/plugins.ts
@@ -1,18 +1,37 @@
+
 export default ({ env }) => ({
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          uploadStream: {},
-          delete: {},
-        },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
-  });
-  
+  },
+
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", 'smtp.example.com'),
+        port: env("SMTP_PORT", 1025),
+        auth: {
+          user: env("SMTP_USER", ""),
+          pass: env("SMTP_PASS", ""),
+        },
+        ignoreTLS: true,
+      },
+      settings: {
+        defaultFrom: env("EMAIL_FROM", "no-reply@tu-dominio.com"),
+        defaultReplyTo: env("EMAIL_REPLY_TO", "no-reply@tu-dominio.com"),
+      },
+    },
+  },
+});
