59cc1e80a77d0781e4ff7dc6a6c4b9c16f3bef63
Carlos <dominguez5493.cd@gmail.com>
2025-03-16 10:46:58 -0500

    setting schema for user

--- api/types/generated/contentTypes.d.ts
+++ api/types/generated/contentTypes.d.ts
@@ -894,7 +894,6 @@ export interface PluginUsersPermissionsUser
   };
   options: {
     draftAndPublish: false;
-    timestamps: true;
   };
   attributes: {
     blocked: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
@@ -908,6 +907,7 @@ export interface PluginUsersPermissionsUser
       Schema.Attribute.SetMinMaxLength<{
         minLength: 6;
       }>;
+    image: Schema.Attribute.Media<'files' | 'images'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
